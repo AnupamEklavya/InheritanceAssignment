a51707b39befaba8574be2186c6cfc8cb0c9d3ae
Anupam Eklavya <Anupam Eklavya@LAPTOP-DJU7DHMU>
2023-02-17 02:31:40 +0530

    Add project files.

---
+++ InheritanceAssignment/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceAssignment
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Phone { get; set; }
+        public Person( int id,string name, string address, double phone)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Address = address;
+            this.Phone = phone;
+        }
+        public virtual void Details()
+        {
+            Console.WriteLine("Id :" + Id);
+            Console.WriteLine("Name :" + Name);
+            Console.WriteLine("Address :" + Address);
+            Console.WriteLine("Phone Number  :" + Phone);
+        }
+    }
+}
---
+++ InheritanceAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace InheritanceAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student =
+            new Student(1, "Anupam", "Bhopal", 765757876, "CS-1", 87, "A+", 60000);
+            Console.WriteLine("Student Details :");
+            Console.WriteLine("-----------------");
+            student.Details();
+            Console.WriteLine("=====================");
+            Console.WriteLine();
+            
+
+            TeachingStaff teachingstaff = 
+            new TeachingStaff(101, "Akshay", "Bhopal", 76976868, "Faculty", 40000, "PHD", "DSA");
+            Console.WriteLine("Teaching Staff Details :");
+            Console.WriteLine("------------------------");
+            teachingstaff.Details();
+            Console.WriteLine("=====================");
+            Console.WriteLine();
+
+            NonTeachingStaff nonteachingstaff = 
+            new NonTeachingStaff(201, "Danish", "Mumbai", 65757588, "HOD", 100000, "CSE",1001);
+            Console.WriteLine("Non Teaching Staff Details :");
+            Console.WriteLine("----------------------------");
+            nonteachingstaff.Details();
+            Console.WriteLine();
+        }
+    }
+}
